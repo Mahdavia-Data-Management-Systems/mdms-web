d0e5b7e3c9d6b6f682c059d36e18d2c584c930c0
noormahdi <noor.mahdi@live.com>
2026-08-27 00:44:54 +1200

    fix: update NEXT_PUBLIC_ENTRA_AUTHORITY to use tenant domain dynamically

--- infra/configurations/prod/main.tf
+++ infra/configurations/prod/main.tf
@@ -13,7 +13,7 @@ module "swa" {
 
   app_settings = {
     "NEXT_PUBLIC_ENTRA_CLIENT_ID" = "42b3e442-6a5f-4a2b-bacd-99bc68fe4f68"
-    "NEXT_PUBLIC_ENTRA_AUTHORITY" = "https://mahdavisonline.com/${data.tfe_outputs.core.values.tenant_id}"
+    "NEXT_PUBLIC_ENTRA_AUTHORITY" = "https://${split(".", data.tfe_outputs.core.values.tenant_domain)[0]}.ciamlogin.com/${data.tfe_outputs.core.values.tenant_id}"
   }
 }
 
